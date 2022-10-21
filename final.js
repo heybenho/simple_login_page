6579db5a19d6ba21ab4abcde394de456a496bf0a
Ben Ho <heybenho@gmail.com>
2022-10-21 00:23:47 -0700

    Updates through Part 3.

--- final.js
+++ final.js
@@ -3,10 +3,20 @@ let divReg, divMain;
 document.addEventListener("DOMContentLoaded", function() {  // Function executes when document goes from loading to interactive.
     divReg = document.getElementById("divRegistered");
     divMain = document.getElementById("divMain");
-    divMain.addEventListener("focus", fHandleEnter, true);  // When in focus, change background to yellow.
-    divMain.addEventListener("blur", fHandleExit, true);  // When out of focus, reset background.
-
+    // divMain.addEventListener("focus", fHandleEnter, true);  // When in focus, change background to yellow.
+    // divMain.addEventListener("blur", fHandleExit, true);  // When out of focus, reset background.
+    $("input").focus(function() {$(this).css("background-color", "yellow"); });  // Same as the addEventListener line, but with jQuery.
+    $("input").blur(function () {$(this).css("background-color", ""); });  // Same as the addEventListener line, but with jQuery.
     fProcessForm();
+
+    let inputElements = document.querySelectorAll("#frmRegister input[type=text], input[type=password]");  // Selects all input texts and passwords.
+    console.log(inputElements);  // Checks to see all input texts and passwords are selected.
+
+    let spanElements = document.querySelectorAll("form span");  // Selects all span elements within the form.
+    console.log(spanElements);  // Checks to see all span elements within the form are selected.
+
+    inputElements[2].addEventListener("blur", function() {fCompareInputs(inputElements[1].value, inputElements[2].value, spanElements[2])},true);
+    inputElements[4].addEventListener("blur", function() {fCompareInputs(inputElements[3].value, inputElements[4].value, spanElements[4])},true);
 })
 
 function fHandleEnter(evt) {
@@ -32,3 +42,20 @@ function fProcessForm() {
         divReg.style.display = "none";
     };
 }
+
+function fCompareInputs(value1, value2, display) {
+    console.log(value1, value2);
+    if (value1.length==0 || value2.length==0) {
+        display.innerHTML = "";
+        display.style = "";
+    } else {
+        if (value1 == value2) {
+            display.innerHTML = "Entries match";
+            display.style.backgroundColor = "green";
+        } else {
+            display.innerHTML = "Entries do not match";
+            display.style.backgroundColor = "red";
+        }
+    }
+}
+
